ee3d28a5a429d46447df8605a12bacca9d7e1073
Eric Olson <eric.rob.olson@gmail.com>
2018-03-25 14:02:54 -0700

    Added in case for Ace to 5 straight.

--- PokerRules.py
+++ PokerRules.py
@@ -16,37 +16,50 @@ def rank_hand(handString):
 		
 	suits = [s[1] for r,s in enumerate(hand)]
 	ranks = sorted([s[0] for r,s in enumerate(hand)], key=lambda i:rankvalues[i], reverse=True)
-	
+
 	max_rank = rankvalues[ranks[0]]
 	min_rank = rankvalues[ranks[-1]]
 	
-	straight = (max_rank - min_rank) == 4 and len(set(ranks)) == 5
+	#a_to_five_straight = max_rank == 14 and min_rank == 2 and max_rank - min_rank == 12 and len(set(ranks)) == 5
+	
+	a_to_five_straight = ['A','5','4','3','2'] == ranks
+	
+	straight = (max_rank - min_rank) == 4 and len(set(ranks)) == 5 or a_to_five_straight
 	flush = len(set(suits)) == 1
+
+
+	def get_rank_values(cards):
+		if a_to_five_straight:
+			return [5,4,3,2,1]	
+		
+		return [rankvalues[s] for s in cards]
 	
 	def of_a_kind(numCards, butnot=None):	
 		if butnot == None:
 			return [r for r in ranks if ranks.count(r) == numCards][:numCards]
 		
 		return [r for r in ranks if ranks.count(r) == numCards and r not in butnot]
-		
-		
-	if straight and flush: return CONST_STRAIGHT_FLUSH, ranks
+					
+	if straight and flush: return CONST_STRAIGHT_FLUSH, get_rank_values(ranks)
 	
 	kind4 = of_a_kind(4)
 	kind1 = of_a_kind(1)
 		
-	if kind4: return CONST_FOUR_OF_A_KIND, kind4, kind1
+	if kind4: return CONST_FOUR_OF_A_KIND, get_rank_values(kind4), get_rank_values(kind1)
 	
 	kind3 = of_a_kind(3)
 	kind2 = of_a_kind(2)
 		
-	if kind3 and kind2: return CONST_FULL_HOUSE, kind3, kind2
-	if flush: return CONST_FLUSH, ranks
-	if straight: return CONST_STRAIGHT, ranks
-	if kind3: return CONST_THREE_OF_A_KIND, kind3, [card for card in ranks if card not in kind3]
+	if kind3 and kind2: return CONST_FULL_HOUSE, get_rank_values(kind3), get_rank_values(kind2)
+	if flush: return CONST_FLUSH, get_rank_values(ranks)
+	if straight: return CONST_STRAIGHT, get_rank_values(ranks)
+	if kind3: return CONST_THREE_OF_A_KIND, get_rank_values(kind3), get_rank_values([card for card in ranks if card not in kind3])
 	
 	secondKind2 = of_a_kind(2, kind2)
 	
-	if kind2 and secondKind2: return CONST_TWO_PAIR, kind2, secondKind2, kind1
-	if kind2: return CONST_PAIR, kind2, [card for card in ranks if card not in kind2]	
-	return CONST_HIGH_CARD, ranks
+	if kind2 and secondKind2: return CONST_TWO_PAIR, get_rank_values(kind2), get_rank_values(secondKind2), get_rank_values(kind1)
+	if kind2: return CONST_PAIR, get_rank_values(kind2), get_rank_values([card for card in ranks if card not in kind2]	)
+	return CONST_HIGH_CARD, get_rank_values(ranks)
+	
+	
+rank_hand('Tc,9d,8s,9s,Ac')
--- test_hand_ranker.py
+++ test_hand_ranker.py
@@ -1,53 +1,59 @@
 import PokerRules
 
 def test_rank_hand_Ad2c3h6d8s_returns_1_A8632():
-	expected = PokerRules.CONST_HIGH_CARD, ['A','8','6','3','2']
+	expected = PokerRules.CONST_HIGH_CARD, [14,8,6,3,2]
 	actual = PokerRules.rank_hand("Ad,2c,3h,6d,8s")
 	assert actual == expected
 	
 	
 def test_rank_hand_AsKsQsJsTs_returns_9_AKQJT():
-	expected = PokerRules.CONST_STRAIGHT_FLUSH, ['A','K','Q','J','T']
+	expected = PokerRules.CONST_STRAIGHT_FLUSH, [14,13,12,11,10]
 	actual = PokerRules.rank_hand("As,Ks,Qs,Js,Ts")
 	assert actual == expected
 	
 def test_rank_hand_9c8c7c6c5c_returns_9_98765():
-	expected = PokerRules.CONST_STRAIGHT_FLUSH, ['9','8','7','6','5']
+	expected = PokerRules.CONST_STRAIGHT_FLUSH, [9,8,7,6,5]
 	actual = PokerRules.rank_hand("9c,8c,7c,6c,5c")
 	assert actual == expected
 		
 def test_rank_hand_4c4d4h4sAc_returns_8_4444A():
-	expected = PokerRules.CONST_FOUR_OF_A_KIND, ['4','4','4','4'], ['A']
+	expected = PokerRules.CONST_FOUR_OF_A_KIND, [4,4,4,4], [14]
 	actual = PokerRules.rank_hand("4c,4d,4h,4s,Ac")
 	assert actual == expected
 		
 def test_rank_hand_4c4d4hAdAc_returns_7_444AA():
-	expected = PokerRules.CONST_FULL_HOUSE, ['4','4','4'], ['A','A']
+	expected = PokerRules.CONST_FULL_HOUSE, [4,4,4], [14,14]
 	actual = PokerRules.rank_hand("4c,4d,4h,Ad,Ac")
 	assert actual == expected
 	
 def test_rank_hand_9c2cKcAc6c_returns_6_AK962():
-	expected = PokerRules.CONST_FLUSH, ['A','K','9','6','2']
+	expected = PokerRules.CONST_FLUSH, [14,13,9,6,2]
 	actual = PokerRules.rank_hand("9c,2c,Kc,Ac,6c")
 	assert actual == expected
 	
 def test_rank_hand_9c8c7c6c5d_returns_5_98765():
-	expected = PokerRules.CONST_STRAIGHT, ['9','8','7','6','5']
+	expected = PokerRules.CONST_STRAIGHT, [9,8,7,6,5]
 	actual = PokerRules.rank_hand("9c,8c,7c,6c,5d")
 	assert actual == expected
 			
 def test_rank_hand_4c4d4hAd2c_returns_4_444A2():
-	expected = PokerRules.CONST_THREE_OF_A_KIND, ['4','4','4'], ['A','2']
+	expected = PokerRules.CONST_THREE_OF_A_KIND, [4,4,4], [14,2]
 	actual = PokerRules.rank_hand("4c,4d,4h,Ad,2c")
 	assert actual == expected
 
 def test_rank_hand_4c4dAhAd2c_returns_3_44AA2():
-	expected = PokerRules.CONST_TWO_PAIR, ['A','A'],['4','4'],['2']
+	expected = PokerRules.CONST_TWO_PAIR, [14,14],[4,4],[2]
 	actual = PokerRules.rank_hand("4c,4d,Ah,Ad,2c")
 	assert actual == expected
 
 	
 def test_rank_hand_3c5dAhAd2c_returns_2_AA532():
-	expected = PokerRules.CONST_PAIR, ['A','A'],['5','3','2']
+	expected = PokerRules.CONST_PAIR, [14,14],[5,3,2]
 	actual = PokerRules.rank_hand("3c,5d,Ah,Ad,2c")
 	assert actual == expected
+
+def test_rank_hand_Ac2c3c4c5c_returns_9_54321():
+	expected = PokerRules.CONST_STRAIGHT_FLUSH, [5,4,3,2,1]
+	actual = PokerRules.rank_hand("Ac,2c,3c,4c,5c")
+	assert actual == expected
+		
