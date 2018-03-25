69d7f2aecec679f8bcd0fbd31fe00b41eda2d282
Eric Olson <eric.rob.olson@gmail.com>
2018-03-25 13:31:00 -0700

    Refactored handstrength calculator.

--- PokerRules.py
+++ PokerRules.py
@@ -1,37 +1,6 @@
 rankvalues = dict((r,i) 
                    for i,r in enumerate('..23456789TJQKA'))
 
-# def poker(hand):
-#     hand = hand.split()
-# 
-#     suits = [s for r,s in hand]
-#     ranks = sorted([rankvalues[[i][/i]r] for r,s in hand])
-#     ranks.reverse()
-#     flush = len(set(suits)) == 1
-#     straight = (max(ranks)-min(ranks))==4 
-#                 and len(set(ranks))==5
-# 
-#     def kind(n, butnot=None):
-#         return some(r for r in ranks
-#                     if ranks.count(r) == n and r != butnot)
-# 
-# 
-#     if straight and flush: return 9, ranks
-#     if kind(4): return 8, kind(4), kind(1)
-#     if kind(3) and kind(2): return 7, kind(3), kind(2)
-#     if flush: return 6, ranks
-#     if straight: return 5, ranks
-#     if kind(3): return 4, kind(3), ranks
-#     if kind(2) and kind(2, kind(2)): 
-#         return 3, kind(2), kind(2, kind(2)), ranks
-#     if kind(2): return 2, kind(2), ranks
-#     return 1, ranks
-# 
-# print sum(poker(line[0:14]) > poker(line[15:29])
-#                  for line in file("poker.txt"))
-# 
-# define poker hands like: 
-
 CONST_STRAIGHT_FLUSH = 9
 CONST_FOUR_OF_A_KIND = 8
 CONST_FULL_HOUSE = 7
@@ -62,12 +31,22 @@ def rank_hand(handString):
 		
 		
 	if straight and flush: return CONST_STRAIGHT_FLUSH, ranks
-	if of_a_kind(4): return CONST_FOUR_OF_A_KIND, of_a_kind(4), of_a_kind(1)
-	if of_a_kind(3) and of_a_kind(2): return CONST_FULL_HOUSE, of_a_kind(3), of_a_kind(2)
+	
+	kind4 = of_a_kind(4)
+	kind1 = of_a_kind(1)
+		
+	if kind4: return CONST_FOUR_OF_A_KIND, kind4, kind1
+	
+	kind3 = of_a_kind(3)
+	kind2 = of_a_kind(2)
+		
+	if kind3 and kind2: return CONST_FULL_HOUSE, kind3, kind2
 	if flush: return CONST_FLUSH, ranks
 	if straight: return CONST_STRAIGHT, ranks
-	if of_a_kind(3): return CONST_THREE_OF_A_KIND, of_a_kind(3), [card for card in ranks if card not in of_a_kind(3)]
-	if of_a_kind(2) and of_a_kind(2, of_a_kind(2)): return CONST_TWO_PAIR, of_a_kind(2), of_a_kind(2, of_a_kind(2)), of_a_kind(1)
-	if of_a_kind(2): return CONST_PAIR, of_a_kind(2), [card for card in ranks if card not in of_a_kind(2)]	
+	if kind3: return CONST_THREE_OF_A_KIND, kind3, [card for card in ranks if card not in kind3]
+	
+	secondKind2 = of_a_kind(2, kind2)
+	
+	if kind2 and secondKind2: return CONST_TWO_PAIR, kind2, secondKind2, kind1
+	if kind2: return CONST_PAIR, kind2, [card for card in ranks if card not in kind2]	
 	return CONST_HIGH_CARD, ranks
-
